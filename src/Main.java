549e50d88c80e0d93a87c827b319623d654b7727
Gim128 <gimhanpabasara4@gmail.com>
2021-10-08 11:45:11 +0530

    initialized project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * -------------------------------------------------------------------------------------------
+ *  Copyright (c) DREAMLABS-Solutions. All rights reserved.
+ *  Licensed under the DLS Licensed. See License.txt in the project root  for License information.
+ * ----------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @author GImhan Pabasara <gimhanpabasara4@gmail.com> (www.gimhanpabasara.tk)
+ * @since 10/8/2021
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public static int calculate(int age){
+        return 105;
+    }
+}
